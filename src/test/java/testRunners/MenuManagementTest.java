81de21b672e9c8e2e782f12f650b85324492bf7d
Tutku Ayavefe <tutkuayavefe@gmail.com>
2023-06-22 22:31:15 +0200

    test runner is added

---
+++ src/test/java/testRunners/MenuManagementTest.java
@@ -0,0 +1,13 @@
+package testRunners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/features",
+        glue = "steps",
+        plugin = {"pretty"})
+public class MenuManagementTest {
+}
